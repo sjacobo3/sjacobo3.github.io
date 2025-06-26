556e317f6836e7a663c0178fec9391b8d165d743
Samara Jacobo <sjacobo3@nd.edu>
2025-06-26 15:39:02 -0400

    restyle

--- script.js
+++ script.js
@@ -7,13 +7,14 @@ fetch(`https://api.github.com/users/${username}/repos`)
     .then(repos => {
         repos.forEach(repo => {
             // only show projects that have a description
-            if (repo.description !== null) {
+            if (repo.description !== null && repo.homepage != null) {
                 const card = document.createElement("div");
                 card.className = "project-card";
                 card.innerHTML = `
                     <h5 class="project-title">${repo.name}</h3>
-                    <p class="project-description">${repo.description || "No description provided."}</p>
+                    <p class="project-description">${repo.description}</p>
                     <a href="${repo.html_url}" target="_blank" class="btn btn-primary">View on Github</a>
+                    <a href="${repo.homepage}" target="_blank" class="btn btn-secondary">Go to Website</a>
                 `;
                 container.appendChild(card);
             }
