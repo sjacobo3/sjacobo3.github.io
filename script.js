7d79ab4b155ca6285f80026e392ae26ef6a3cc8f
Samara Jacobo <sjacobo3@nd.edu>
2025-06-26 00:21:40 -0400

    restyle home

---
+++ script.js
@@ -0,0 +1,10 @@
+git = "https://github.com/sjacobo3"
+linkedin = "https://www.linkedin.com/in/samara-jacobo/"
+
+function openGit() {
+    window.location.href = git;
+}
+
+function openLinkedIn() {
+    window.location.href = linkedin;
+}
