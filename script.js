cc96781da8819e2deae42b4dd8c097acbcb7fed3
Samara Jacobo <sjacobo3@nd.edu>
2025-07-07 22:33:16 -0400

    modify github projects fetch

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ fetch(`https://api.github.com/users/${username}/repos`)
     .then(repos => {
         repos.forEach(repo => {
             // only show projects that have a description
-            if (repo.description !== null && repo.homepage != null) {
+            if (repo.description) {
                 const card = document.createElement("div");
                 card.className = "project-card";
                 card.innerHTML = `
-                    <h5 class="project-title">${repo.name}</h3>
+                    <h5 class="project-title">${repo.name}</h5>
                     <p class="project-description">${repo.description}</p>
                     <a href="${repo.html_url}" target="_blank" class="btn btn-primary">View on Github</a>
-                    <a href="${repo.homepage}" target="_blank" class="btn btn-secondary">Go to Website</a>
+                    ${repo.homepage ? `<a href="${repo.homepage}" target="_blank" class="btn btn-secondary">Go to Website</a>` : ""}
                 `;
                 container.appendChild(card);
             }
